2e548a0dc47c4ae52afe8e1a069239576813007b
0x0than <136307533+revothan@users.noreply.github.com>
2025-03-20 11:17:22 +0700

    Add Tailwind configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: ["class"],
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     container: {
