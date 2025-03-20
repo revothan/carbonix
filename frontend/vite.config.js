c0b0c04186a7f893424860d611703da8904caba0
0x0than <136307533+revothan@users.noreply.github.com>
2025-03-21 03:29:50 +0700

    Update Vite config to use tsconfig-paths plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,6 +18,7 @@ export default defineConfig({
       },
       include: '**/*.svg',
     }),
+    tsconfigPaths(), // Add tsconfig paths plugin
   ],
   resolve: {
     alias: {
