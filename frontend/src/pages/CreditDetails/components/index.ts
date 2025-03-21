35600c8c4ac751df81fc2e17ae4bb8f552e56d5b
0x0than <136307533+revothan@users.noreply.github.com>
2025-03-22 02:47:49 +0700

    Add components index file

---
+++ frontend/src/pages/CreditDetails/components/index.ts
@@ -0,0 +1,7 @@
+export { default as ProjectImageGallery } from './ProjectImageGallery';
+export { default as PurchaseCard } from './PurchaseCard';
+export { default as OverviewTab } from './OverviewTab';
+export { default as ProjectDetailsTab } from './ProjectDetailsTab';
+export { default as VerificationTab } from './VerificationTab';
+export { default as ImpactTab } from './ImpactTab';
+export { default as ProjectBreadcrumbs } from './ProjectBreadcrumbs';
