56dd56f4b0793c32f6cc0964bd22a196cbc3bcbd
0x0than <136307533+revothan@users.noreply.github.com>
2025-03-22 03:27:49 +0700

    Add components index for Wallet

---
+++ frontend/src/pages/Wallet/components/index.ts
@@ -0,0 +1,5 @@
+export { default as WalletHeader } from './WalletHeader';
+export { default as TransactionHistory } from './TransactionHistory';
+export { default as DepositForm } from './DepositForm';
+export { default as WithdrawForm } from './WithdrawForm';
+export { default as ConvertForm } from './ConvertForm';
