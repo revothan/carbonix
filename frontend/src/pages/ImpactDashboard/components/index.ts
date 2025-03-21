6b1c86530866581eccb5dd8dee6f85e0c62f4ec8
0x0than <136307533+revothan@users.noreply.github.com>
2025-03-22 02:53:05 +0700

    Add components barrel file

---
+++ frontend/src/pages/ImpactDashboard/components/index.ts
@@ -0,0 +1,7 @@
+export { default as DashboardHeader } from './DashboardHeader';
+export { default as TimeRangeSelector } from './TimeRangeSelector';
+export { default as ImpactSummary } from './ImpactSummary';
+export { default as ViewTabs } from './ViewTabs';
+export { default as OverviewContent } from './OverviewContent';
+export { default as ProjectsContent } from './ProjectsContent';
+export { default as SDGContent } from './SDGContent';
